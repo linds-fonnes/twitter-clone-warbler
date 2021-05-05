60b2abf2810031b9946b409982b6d49f26d08a69
Lindsey <linds.fonnes@gmail.com>
2021-05-05 14:57:10 -0600

    refactored and added more tests

--- test_user_views.py
+++ test_user_views.py
@@ -11,51 +11,64 @@ from app import app, CURR_USER_KEY
 
 app.config['WTF_CSRF_ENABLED'] = False
 
-db.create_all()
 
 class UserViewTestCase(TestCase):
     """Tests views for users"""
 
     def setUp(self):
-        User.query.delete()
-        Follows.query.delete()
+    
+        db.drop_all()
+        db.create_all()
+
+        self.u1 = User(
+            id = 9999,
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+        self.u2 = User(
+            id = 9998,
+            email="test2@test.com",
+            username="testuser2",
+            password="HASHED_PASSWORD"
+        )
+        self.u3 = User(
+            id = 9997,
+            email="test3@test.com",
+            username="user9997",
+            password="HASHED_PASSWORD"
+        )
+        self.u1_id = self.u1.id
+        self.u2_id = self.u2.id
+        self.u3_id = self.u3.id
+        db.session.add_all([self.u1,self.u2,self.u3])
+        db.session.commit()
 
         self.client = app.test_client()
     
     def tearDown(self):
         db.session.rollback()
     
-    def test_show_followers(self):
-        """when logged in can you see the follower pages for any user?"""
-
-        #set up users and follow each other
-        u1 = User(
-            id = 9999,
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-        u2 = User(
-            id = 9998,
-            email="test2@test.com",
-            username="testuser2",
-            password="HASHED_PASSWORD"
-        )
-        db.session.add_all([u1,u2])
-        db.session.commit()
-
-        follow = Follows(user_being_followed_id=9998, user_following_id=9999)
-        follow2 = Follows(user_being_followed_id=9999,user_following_id=9998)
+    def setUpFollowers(self):
+         #set up users followings
+      
+        follow = Follows(user_being_followed_id=self.u2_id, user_following_id=self.u1_id)
+        follow2 = Follows(user_being_followed_id=self.u1_id,user_following_id=self.u2_id)
         db.session.add_all([follow,follow2])
         db.session.commit()
 
+    def test_show_followers(self):
+        """when logged in can you see the follower pages for any user?"""
+
         #tests that user1's name displays in user2's list of followers and user2's name displays in user1's followers
+        self.setUpFollowers() 
+
         with self.client as c:
             with c.session_transaction() as session:
-                session[CURR_USER_KEY] = 9999
+                session[CURR_USER_KEY] = self.u1_id
 
-            resp = c.get("/users/9998/followers")
-            resp2 = c.get("/users/9999/followers")
+            resp = c.get(f"/users/{self.u2_id}/followers")
+            resp2 = c.get(f"/users/{self.u1_id}/followers")
 
             self.assertEqual(resp.status_code,200)
             self.assertIn("testuser",str(resp.data))
@@ -65,34 +78,14 @@ class UserViewTestCase(TestCase):
     def test_show_following(self):
         """when logged in can you see the following pages for any user?"""
 
-        #set up users and follow each other
-        u1 = User(
-            id = 9999,
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-        u2 = User(
-            id = 9998,
-            email="test2@test.com",
-            username="testuser2",
-            password="HASHED_PASSWORD"
-        )
-        db.session.add_all([u1,u2])
-        db.session.commit()
-
-        follow = Follows(user_being_followed_id=9998, user_following_id=9999)
-        follow2 = Follows(user_being_followed_id=9999,user_following_id=9998)
-        db.session.add_all([follow,follow2])
-        db.session.commit()
-
+        self.setUpFollowers()
          #tests that user1's name displays in user2's list of people following and vice versa
         with self.client as c:
             with c.session_transaction() as session:
-                session[CURR_USER_KEY] = 9999
+                session[CURR_USER_KEY] = self.u1_id
 
-            resp = c.get("/users/9998/following")
-            resp2 = c.get("/users/9999/following")
+            resp = c.get(f"/users/{self.u2_id}/following")
+            resp2 = c.get(f"/users/{self.u1_id}/following")
 
             self.assertEqual(resp.status_code,200)
             self.assertIn("testuser",str(resp.data))
@@ -102,57 +95,80 @@ class UserViewTestCase(TestCase):
     def test_show_followers_no_user(self):
         #tests that a logged out user cannot view user followers of a user
 
-        #set up users and follow each other
-        u1 = User(
-            id = 9999,
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-        u2 = User(
-            id = 9998,
-            email="test2@test.com",
-            username="testuser2",
-            password="HASHED_PASSWORD"
-        )
-        db.session.add_all([u1,u2])
-        db.session.commit()
-
-        follow = Follows(user_being_followed_id=9998, user_following_id=9999)
-        follow2 = Follows(user_being_followed_id=9999,user_following_id=9998)
-        db.session.add_all([follow,follow2])
-        db.session.commit()
+        self.setUpFollowers()
 
         with self.client as c:
-            resp = c.get("/users/9999/followers", follow_redirects=True)
+            resp = c.get(f"/users/{self.u1_id}/followers", follow_redirects=True)
             self.assertEqual(resp.status_code,200)
             self.assertIn("Access unauthorized.", str(resp.data))
 
     def test_show_following_no_user(self):
         #tests that a logged out user cannot view users being followed of a user
 
-        #set up users and follow each other
-        u1 = User(
-            id = 9999,
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-        u2 = User(
-            id = 9998,
-            email="test2@test.com",
-            username="testuser2",
-            password="HASHED_PASSWORD"
-        )
-        db.session.add_all([u1,u2])
-        db.session.commit()
-
-        follow = Follows(user_being_followed_id=9998, user_following_id=9999)
-        follow2 = Follows(user_being_followed_id=9999,user_following_id=9998)
-        db.session.add_all([follow,follow2])
-        db.session.commit()
+        self.setUpFollowers()
 
         with self.client as c:
-            resp = c.get("/users/9999/following", follow_redirects=True)
+            resp = c.get(f"/users/{self.u1_id}/following", follow_redirects=True)
             self.assertEqual(resp.status_code,200)
-            self.assertIn("Access unauthorized.", str(resp.data))
+            self.assertIn("Access unauthorized.", str(resp.data)) 
+
+    def test_show_users(self):
+        #tests that all users display
+        with self.client as c:
+            with c.session_transaction() as session:
+                session[CURR_USER_KEY] = self.u1_id
+        resp = c.get("/users")
+        self.assertEqual(resp.status_code,200)
+        self.assertIn("testuser", str(resp.data))
+        self.assertIn("testuser2", str(resp.data))
+
+    def test_search_user(self):
+        #tests that search feature displays matching users 
+        with self.client as c:
+            with c.session_transaction() as session:
+                session[CURR_USER_KEY] = self.u1_id
+        resp = c.get("/users?q=test")
+        self.assertEqual(resp.status_code,200)
+        self.assertIn("testuser", str(resp.data))
+        self.assertIn("testuser", str(resp.data))  
+        self.assertNotIn("user9997",str(resp.data)) 
+
+    def test_user_profile(self):
+        #tests that specific user's info displays on their profile page
+        with self.client as c:
+            with c.session_transaction() as session:
+                session[CURR_USER_KEY] = self.u1_id
+
+        resp = c.get("/users/9997")
+        self.assertEqual(resp.status_code,200)
+        self.assertIn("user9997",str(resp.data))
+
+    def test_add_follow(self):
+        #tests that a user can follow another user
+        with self.client as c:
+            with c.session_transaction() as session:
+                session[CURR_USER_KEY] = self.u1_id
+
+        resp = c.post(f"/users/follow/{self.u3_id}", follow_redirects=True)  
+        self.assertEqual(resp.status_code,200) 
+
+        resp2 = c.get(f"/users/{self.u1_id}/following")
+        self.assertIn("user9997",str(resp2.data))
+
+    def test_remove_follow(self):
+        #tests that a user can unfollow a user 
+        self.setUpFollowers()
+        with self.client as c:
+            with c.session_transaction() as session:
+                session[CURR_USER_KEY] = self.u1_id
+        
+        resp = c.post(f"/users/stop-following/{self.u2_id}", follow_redirects=True)
+        self.assertEqual(resp.status_code,200)
+
+        resp2 = c.get(f"/users/{self.u1_id}/following")
+        self.assertNotIn("testuser2", str(resp2.data))
+    
+        
+
+
+
